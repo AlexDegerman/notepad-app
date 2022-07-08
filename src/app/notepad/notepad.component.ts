a510eadcb200fa5fe22b93c5e9aa93f8ef08ea8c
AlexDegerman <alex.degerman.dev@gmail.com>
2022-07-08 03:52:20 +0300

    add localstorage to store and load notes

--- src/app/notepad/notepad.component.ts
+++ src/app/notepad/notepad.component.ts
@@ -8,27 +8,38 @@ export class Notes {
 @Component({
   selector: 'app-notepad',
   templateUrl: './notepad.component.html',
-  styleUrls: ['./notepad.component.css']
+  styleUrls: ['./notepad.component.css'],
 })
 export class NotepadComponent implements OnInit {
-  title = "";
-  content = ""
+  title = '';
+  content = '';
   notes: Notes[] = [];
 
- 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.loadData();
   }
 
-  createNewNote(){
+  createNewNote(event: any) {
     const notes: Notes = {
       title: this.title,
-      content: this.content
+      content: this.content,
     };
-
+    console.log(event);
+    localStorage.setItem(event, JSON.stringify(notes));
     this.notes.push(notes);
-    this.title = "";
-    this.content = "";
+    this.title = '';
+    this.content = '';
+  }
+
+  loadData() {
+    var values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
+    while (i--) {
+      let datus = localStorage[keys[i]];
+      this.notes.push(JSON.parse(datus));
+    }
   }
 }
